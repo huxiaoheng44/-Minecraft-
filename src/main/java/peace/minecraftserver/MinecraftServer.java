288445c1e3d3691274fb4aa7e4fb7151d9769549
wzharies <861613130@qq.com>
2020-07-02 09:16:06 +0800

    empty project

---
+++ src/main/java/peace/minecraftserver/MinecraftServer.java
@@ -0,0 +1,17 @@
+package peace.minecraftserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftServer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
