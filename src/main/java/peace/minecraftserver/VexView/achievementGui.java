52ba67fd0dd59bf153a99778a6f0d3aa48c5b09c
tony <44961646+Donjoker@users.noreply.github.com>
2020-07-16 17:29:33 +0800

    fix bug plus

--- src/main/java/peace/minecraftserver/VexView/achievementGui.java
+++ src/main/java/peace/minecraftserver/VexView/achievementGui.java
@@ -34,7 +34,7 @@ public class achievementGui {
         //参数 x y 内容 字体大小
         vexComponentsList.add(new VexText(30,70,Arrays.asList("连续签到五天以上","可获得此成就"),1));
         vexComponentsList.add(new VexButton("buy1","佩戴","[local]button.png","[local]button_.png",50,225,35,25,player1 -> {
-           if(VexViewAPI.getPlayerTags(player)==null) {
+           if(VexViewAPI.getPlayerTags(player)==null||VexViewAPI.getPlayerTags(player).isEmpty()) {
                MinecraftServer.plugin.getLogger().info("kong");
                if (Integer.valueOf(MainGui.getConday(player)) > 5) {
 
@@ -45,10 +45,14 @@ public class achievementGui {
                } else {
                    player.sendMessage("连续签到要大于五天");
                }
+           }else {
+               MinecraftServer.plugin.getLogger().info(VexViewAPI.getPlayerTags(player).toString());
+               player.sendMessage("正在佩戴其他头衔");
            }
         }));
         vexComponentsList.add(new VexButton("remove_renqi","卸下","[local]button.png","[local]button_.png",50,180,35,25,player1 -> {
            VexViewAPI.removePlayerTag(player,"renqi_achievement");
+            VexViewAPI.getPlayerTags(player).clear();
         }));
 
 
@@ -57,7 +61,7 @@ public class achievementGui {
         vexComponentsList.add(new VexImage("[local]inv.png",150,55,90,120));
         vexComponentsList.add(new VexText(155,70,Arrays.asList("等级大于10","可获得此成就"),1));
         vexComponentsList.add(new VexButton("buy2","佩戴","[local]button.png","[local]button_.png",175,225,35,25,player1 -> {
-            if(VexViewAPI.getPlayerTags(player)==null) {
+            if(VexViewAPI.getPlayerTags(player)==null||VexViewAPI.getPlayerTags(player).isEmpty()) {
                 if (player.getLevel() > 10 ) {
                     TagDirection td = new TagDirection(0, 180, 0, true, false);
                     VexImageTag vit = new VexImageTag("lonely_achiv", 0, 3, 0, "[local]Du1.png", 100, 50, 1, 1, td);
@@ -66,10 +70,14 @@ public class achievementGui {
                 } else {
                     player.sendMessage("等级要大于10");
                 }
+            }else {
+                MinecraftServer.plugin.getLogger().info(VexViewAPI.getPlayerTags(player).toString());
+                player.sendMessage("正在佩戴其他头衔");
             }
         }));
         vexComponentsList.add(new VexButton("remove_lonely","卸下","[local]button.png","[local]button_.png",175,180,35,25,player1 -> {
             VexViewAPI.removePlayerTag(player,"lonely_achiv");
+            VexViewAPI.getPlayerTags(player).clear();
         }));
 
         vexComponentsList.add(new VexText(280,25,Arrays.asList("富可敌国"),2));
@@ -78,7 +86,7 @@ public class achievementGui {
 
         vexComponentsList.add(new VexText(275,70,Arrays.asList("金币大于10000","可获得此成就"),1));
         vexComponentsList.add(new VexButton("buy3","佩戴","[local]button.png","[local]button_.png",295,225,35,25,player1 -> {
-            if(VexViewAPI.getPlayerTags(player)==null) {
+            if(VexViewAPI.getPlayerTags(player)==null||VexViewAPI.getPlayerTags(player).isEmpty()) {
                 if (VaultUtil.seemoney(player.getUniqueId()) > 10000) {
                     TagDirection td = new TagDirection(0, 180, 0, true, false);
                     VexImageTag vit = new VexImageTag("rich_achiv", 0, 3, 0, "[local]Fu1.png", 100, 50, 1, 1, td);
@@ -87,10 +95,14 @@ public class achievementGui {
                 } else {
                     player.sendMessage("金币不足10000，无法佩戴");
                 }
+            }else {
+                MinecraftServer.plugin.getLogger().info(VexViewAPI.getPlayerTags(player).toString());
+                player.sendMessage("正在佩戴其他头衔");
             }
         }));
         vexComponentsList.add(new VexButton("remove_rich","卸下","[local]button.png","[local]button_.png",295,180,35,25,player1 -> {
             VexViewAPI.removePlayerTag(player,"rich_achiv");
+            VexViewAPI.getPlayerTags(player).clear();
         }));
         return new VexGui("[local]gui.png",-1,-1,400,270,vexComponentsList);
 
